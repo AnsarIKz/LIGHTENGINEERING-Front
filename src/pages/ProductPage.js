a221cb3956129f7c044ade8c01b6f07ed90f6ed3
AnsarIKz <nansarik@mail.ru>
2023-03-19 16:11:35 +0600

    Product Page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import CommonIntroduction from "../components/CommonIntroduction";
 import Footer from "../components/Footer";
-import { FontHeader } from "../shared/fonts";
+import { FontHeader, FontTitle, FontTitle2 } from "../shared/fonts";
 import {
   Entity,
   StyledContainer,
@@ -12,21 +12,45 @@ import { useParams } from "react-router-dom";
 
 import category from "../shared/store/category";
 import { CatalogNavigation } from "../components/CatalogNavigation";
+import styled from "styled-components";
+import { observer } from "mobx-react-lite";
 
-const ProductDescription = () => {
+const StyledProduct = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const StyledProductImage = styled.img`
+  flex: 1;
+`;
+const StyledProductDescription = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProductDescription = observer(() => {
+  return (
+    <StyledProduct style={{ marginTop: 40 }}>
+      <div>
+        <StyledProductImage src={product.data.image} />
+      </div>
+      <StyledProductDescription>
+        <FontTitle2 color="black">{product.data.name}</FontTitle2>
+      </StyledProductDescription>
+    </StyledProduct>
+  );
+});
+
+const ProductPage = () => {
   let { productId } = useParams();
   useEffect(() => {
     let load = async () => {
       await product.fetchProduct(productId);
-
       category.fetchCatalog(product.data.category);
     };
     load();
   }, []);
-  return <></>;
-};
-
-const ProductPage = () => {
   return (
     <>
       <CommonIntroduction>
